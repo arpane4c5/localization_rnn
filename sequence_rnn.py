8db7f34dd5c274487f2493a7f66b68f349495fc2
arpane4c5 <arpan.e4c5@gmail.com>
2018-06-23 00:12:07 +0530

    Executable code 0.1

--- sequence_rnn.py
+++ sequence_rnn.py
@@ -17,18 +17,19 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
-import pickle
+import time
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 torch.manual_seed(777)  # reproducibility
 
 # Local Paths
 LABELS = "/home/hadoop/VisionWorkspace/Cricket/scripts/supporting_files/sample_set_labels/sample_labels_shots/ICC WT20"
-DATASET = "/home/hadoop/VisionWorkspace/VideoData/sample_cricket"
+DATASET = "/home/hadoop/VisionWorkspace/VideoData/sample_cricket/ICC WT20"
 
 # Server Paths
 if os.path.exists("/opt/datasets/cricket/ICC_WT20"):
     LABELS = "/home/arpan/VisionWorkspace/shot_detection/supporting_files/sample_set_labels/sample_labels_shots/ICC WT20"
-    DATASET = "/opt/datasets/cricket"
+    DATASET = "/opt/datasets/cricket/ICC WT20"
 
 num_classes = 2
 input_size = 1  # one-hot size
@@ -38,36 +39,101 @@ sequence_length = 4404  # |ihello| == 6
 num_layers = 1  # one-layer rnn
 
 
-class RNN(nn.Module):
+class RNNClassifier(nn.Module):
+    # Our model
 
-    def __init__(self, num_classes, input_size, hidden_size, num_layers):
-        super(RNN, self).__init__()
-
-        self.num_classes = num_classes
-        self.num_layers = num_layers
-        self.input_size = input_size
+    def __init__(self, input_size, hidden_size, output_size, n_layers=1, bidirectional=False):
+        super(RNNClassifier, self).__init__()
         self.hidden_size = hidden_size
-        self.sequence_length = sequence_length
+        self.n_layers = n_layers
+        self.n_directions = int(bidirectional) + 1
 
-        self.rnn = nn.RNN(input_size=self.input_size, hidden_size=self.hidden_size, \
-                          batch_first=True)
+        #self.embedding = nn.Embedding(input_size, hidden_size)
+        #self.rnn = nn.RNN(input_size=input_size,
+        #                  hidden_size=hidden_size, batch_first=True)
+        self.gru = nn.GRU(input_size, hidden_size, n_layers, batch_first=True, 
+                          bidirectional=bidirectional)
+        self.fc = nn.Linear(hidden_size, output_size)
+        #self.soft = nn.Softmax()
 
-    def forward(self, x):
-        # Initialize hidden and cell states
-        # (num_layers * num_directions, batch, hidden_size) for batch_first=True
-        h_0 = Variable(torch.zeros(
-            self.num_layers, x.size(0), self.hidden_size))
+    def forward(self, input):
+        # Note: we run this all at once (over the whole input sequence)
+        # input shape: B x S (input size)
+        # transpose to make S(sequence) x B (batch)
+        #input = input.t()
+        #batch_size = input.size(1)
+        batch_size = input.size(0)
+        seq_len = input.size(1)
+        
+        print "Seq Len : {} :: Batch size : {}".format(seq_len, batch_size)
 
-        # Reshape input
-        x.view(x.size(0), self.sequence_length, self.input_size)
+        # Make a hidden
+        hidden = self._init_hidden(batch_size)
 
-        # Propagate input through RNN
-        # Input: (batch, seq_len, input_size)
-        # h_0: (num_layers * num_directions, batch, hidden_size)
+        # Embedding S x B -> S x B x I (embedding size)
+        #embedded = self.embedding(input.view(seq_len, batch_size, -1))
+        # B X S X 1152
+        embedded = input
 
-        out, _ = self.rnn(x, h_0)
-        return out.view(-1, num_classes-1)
+        # Pack them up nicely
+        #gru_input = pack_padded_sequence(
+        #    embedded, seq_lengths.data.cpu().numpy())
 
+        # To compact weights again call flatten_parameters().
+        self.gru.flatten_parameters()
+        #output, hidden = self.rnn(embedded, hidden)
+        output, hidden = self.gru(embedded, hidden)
+
+        # Use the last layer output as FC's input
+        # No need to unpack, since we are going to use hidden
+        return self.fc(output.contiguous().view(-1, self.hidden_size))        
+        #fc_output = self.fc(hidden[-1])
+        #return fc_output
+
+    def _init_hidden(self, batch_size):
+         #* self.n_directions
+        hidden = torch.zeros(self.n_layers * self.n_directions, batch_size, \
+                             self.hidden_size)
+        return create_variable(hidden)
+
+
+#class RNN(nn.Module):
+#
+#    def __init__(self, num_classes, input_size, hidden_size, num_layers):
+#        super(RNN, self).__init__()
+#
+#        self.num_classes = num_classes
+#        self.num_layers = num_layers
+#        self.input_size = input_size
+#        self.hidden_size = hidden_size
+#        self.sequence_length = sequence_length
+#
+#        self.rnn = nn.RNN(input_size=self.input_size, hidden_size=self.hidden_size, \
+#                          batch_first=True)
+#
+#    def forward(self, x):
+#        # Initialize hidden and cell states
+#        # (num_layers * num_directions, batch, hidden_size) for batch_first=True
+#        h_0 = Variable(torch.zeros(
+#            self.num_layers, x.size(0), self.hidden_size))
+#
+#        # Reshape input
+#        x.view(x.size(0), self.sequence_length, self.input_size)
+#
+#        # Propagate input through RNN
+#        # Input: (batch, seq_len, input_size)
+#        # h_0: (num_layers * num_directions, batch, hidden_size)
+#
+#        out, _ = self.rnn(x, h_0)
+#        return out.view(-1, num_classes-1)
+
+
+def create_variable(tensor):
+    # Do cuda() before wrapping with variable
+    #if torch.cuda.is_available():
+    #    return Variable(tensor.cuda())
+    #else:
+    return Variable(tensor)
 
 # Split the dataset files into training, validation and test sets
 def split_dataset_files():
@@ -193,13 +259,6 @@ def extract_hog_from_video(srcVid):
     return seq
 
 
-## Set the parameter candidates
-#parameter_candidates = [
-#  {'C': [1], 'kernel': ['linear']}]
-# # {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
-##]
-
-
 # Visualize the positive and negative samples
 # Params: list of numpy arrays of size nFrames-1 x Channels
 def visualize_feature(samples_lst, title="Histogram", bins=300):
@@ -312,9 +371,14 @@ def getNFrames(vid):
 # videoFiles : list/tuple of filenames for the videos (size n)
 # sequences :  list of start frame numbers and end frame numbers 
 # sequences[0] and [1] are torch.LongTensor of size n each.
+# returns a list of lists. Inner list contains a sequence of arrays 
 def getFeatureVectors(datasetpath, videoFiles, sequences):
+    grid_size = 20
+    batch_feats = []
     # Iterate over the videoFiles in the batch and extract the corresponding feature
     for i, videoFile in enumerate(videoFiles):
+        videoFile = videoFile.split('/')[1]
+        vid_feat_seq = []
         # use capture object to get the sequences
         cap = cv2.VideoCapture(os.path.join(datasetpath, videoFile))
         if not cap.isOpened():
@@ -322,8 +386,120 @@ def getFeatureVectors(datasetpath, videoFiles, sequences):
             import sys
             sys.exit(0)
             
+        start_frame = sequences[0][i]
+        end_frame = sequences[1][i]
+        ####################################################    
+        #print "Start Times : {} :: End Times : {}".format(start_frame, end_frame)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+        ret, prev_frame = cap.read()
+        if ret:
+            # convert frame to GRAYSCALE
+            prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+        else:
+            print "Frame not read: {} : {}".format(videoFile, start_frame)
+
+        for stime in range(start_frame+1, end_frame+1):
+            ret, frame = cap.read()
+            if not ret:
+                print "Frame not read : {} : {}".format(videoFile, stime)
+                continue
+            
+            curr_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            #cv2.calcOpticalFlowFarneback(prev, next, pyr_scale, levels, winsize, 
+            #                iterations, poly_n, poly_sigma, flags[, flow])
+            # prev(y,x)~next(y+flow(y,x)[1], x+flow(y,x)[0])
+            flow = cv2.calcOpticalFlowFarneback(prev_frame,curr_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            #print "For frames: ("+str(stime-1)+","+str(stime)+") :: shape : "+str(flow.shape)
+            
+            mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+            # stack sliced arrays along the first axis (2, 12, 16)
+            sliced_flow = np.stack(( mag[::grid_size, ::grid_size], \
+                                    ang[::grid_size, ::grid_size]), axis=0)
+            sliced_flow = sliced_flow.ravel()   # flatten
+            vid_feat_seq.append(sliced_flow.tolist())    # append to list
+            prev_frame = curr_frame
+        cap.release()            
+        batch_feats.append(vid_feat_seq)
         
-        # get feature sequences 
+    return batch_feats
+    
+
+## Train cycle
+#def train(train_loader):
+#    total_loss = 0
+#
+#    for i, (names, countries) in enumerate(train_loader, 1):
+#        input, seq_lengths, target = make_variables(names, countries)
+#        output = classifier(input, seq_lengths)
+#
+#        loss = criterion(output, target)
+#        total_loss += loss.data[0]
+#
+#        classifier.zero_grad()
+#        loss.backward()
+#        optimizer.step()
+#
+#        if i % 10 == 0:
+#            print('[{}] Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.2f}'.format(
+#                time_since(start), epoch,  i *
+#                len(names), len(train_loader.dataset),
+#                100. * i * len(names) / len(train_loader.dataset),
+#                total_loss / i * len(names)))
+#
+#    return total_loss
+
+
+## Testing cycle
+#def test(name=None):
+#    # Predict for a given name
+#    if name:
+#        input, seq_lengths, target = make_variables([name], [])
+#        output = classifier(input, seq_lengths)
+#        pred = output.data.max(1, keepdim=True)[1]
+#        country_id = pred.cpu().numpy()[0][0]
+#        print(name, "is", train_dataset.get_country(country_id))
+#        return
+#
+#    print("evaluating trained model ...")
+#    correct = 0
+#    train_data_size = len(test_loader.dataset)
+#
+#    for names, countries in test_loader:
+#        input, seq_lengths, target = make_variables(names, countries)
+#        output = classifier(input, seq_lengths)
+#        pred = output.data.max(1, keepdim=True)[1]
+#        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+#
+#    print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
+#        correct, train_data_size, 100. * correct / train_data_size))
+
+
+# Inputs: feats: list of lists
+def make_variables(feats, labels):
+    # Create the input tensors and target label tensors
+    #for item in feats:
+        # item is a list with (sequence of) 9 1D vectors (each of 1152 size)
+        
+    feats = torch.Tensor(feats)
+    feats[feats==float("-Inf")] = 0
+    feats[feats==float("Inf")] = 0
+    # Form the target labels 
+    target = []
+#    for i in range(labels[0].size(0)):
+#        if labels[0][i]<5:
+#            target.append(0)
+#        else:
+#            target.append(1)
+            
+    for i in range(labels[0].size(0)):
+        if labels[0][i]>0:
+            target.extend([0]*(labels[0][i]-1) + [1]*labels[1][i])
+        else:
+            target.extend([0]*labels[0][i] + [1]*(labels[1][i]-1))
+    # Form a wrap into a tensor variable as B X S X I
+    return Variable(feats), Variable(torch.Tensor(target))
+
 
 if __name__=="__main__":
     # Divide the samples files into training set, validation and test sets
@@ -350,17 +526,91 @@ if __name__=="__main__":
     hlDataset = VideoDataset(tr_labs, sizes, is_train_set = True)
     print hlDataset.__len__
     
+    # Parameters and DataLoaders
+    HIDDEN_SIZE = 100
+    N_LAYERS = 1
+    BATCH_SIZE = 10
+    N_EPOCHS = 2
+    N_CHARS = 1152      # taking grid_size = 20 get this feature vector size 
+    
+    #test_dataset = NameDataset(is_train_set=False)
+    #test_loader = DataLoader(dataset=test_dataset,
+    #                         batch_size=BATCH_SIZE, shuffle=True)
+    
+    #N_COUNTRIES = len(train_dataset.get_countries())
+        
+    classifier = RNNClassifier(N_CHARS, HIDDEN_SIZE, 1, N_LAYERS)
+    #if torch.cuda.device_count() > 1:
+    #    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    #    # dim = 0 [33, xxx] -> [11, ...], [11, ...], [11, ...] on 3 GPUs
+    #    classifier = nn.DataParallel(classifier)
+
+    #if torch.cuda.is_available():
+    #    classifier.cuda()
+
+    optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001)
+    #criterion = nn.CrossEntropyLoss()
+    m = nn.Sigmoid()
+    criterion = nn.BCELoss()
+
+    start = time.time()
+    print("Training for %d epochs..." % N_EPOCHS)
+    
     # Create a DataLoader object and sample batches of examples. 
     # These batch samples are used to extract the features from videos parallely
-    train_loader = DataLoader(dataset=hlDataset, batch_size=10, shuffle=True)
+    train_loader = DataLoader(dataset=hlDataset, batch_size=BATCH_SIZE, shuffle=True)
 
     print(len(train_loader.dataset))
     for epoch in range(2):
+        total_loss = 0
         for i, (keys, seqs, labels) in enumerate(train_loader):
             # Run your training process
-            print(epoch, i, "keys", keys, "Sequences", seqs, "Labels", labels)
-            feats = getFeatureVectors(keys, seqs)
+            print(epoch, i) #, "keys", keys, "Sequences", seqs, "Labels", labels)
+            feats = getFeatureVectors(DATASET, keys, seqs)
+            #break
+
+            # Training starts here
+            inputs, target = make_variables(feats, labels)
+            output = classifier(inputs)
+
+            loss = criterion(m(output.view(output.size(0))), target)
+            total_loss += loss.data[0]
+
+            classifier.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if i % 10 == 0:
+                print('Train Epoch: {} :: Loss: {:.2f}'.format(epoch, total_loss))
     
+    #####################################################################
+    
+    #    # Prepare the RNN 
+##    # Instantiate RNN model
+#    rnn = RNN(num_classes, input_size, hidden_size, num_layers)
+#    print(rnn)
+#    
+#    # Set loss and optimizer function
+#    # CrossEntropyLoss = LogSoftmax + NLLLoss
+#    criterion = torch.nn.CrossEntropyLoss()
+#    optimizer = torch.optim.Adam(rnn.parameters(), lr=0.1)
+#    
+#    # Train the model
+#    for epoch in range(10):
+#        outputs = rnn(inputs)
+#        optimizer.zero_grad()
+#        loss = criterion(outputs, labels)
+#        loss.backward()
+#        optimizer.step()
+#        _, idx = outputs.max(1)
+#        idx = idx.data.numpy()
+#        #result_str = [idx2char[c] for c in idx.squeeze()]
+#        
+#        print("epoch: %d, loss: %1.3f" % (epoch + 1, loss.data[0]))
+#        #print("Predicted string: ", ''.join(idx))
+#    
+#    print("Learning finished!")    
+
     #####################################################################
     
 #    # get the positions where cuts exist for training set
@@ -402,35 +652,8 @@ if __name__=="__main__":
 #    # Convert to shape (1, 4404, 1)  (Feature Vector size is 1)
 #    inputs = Variable(torch.Tensor(np.expand_dims(x_data, 0)))
 #    labels = Variable(torch.LongTensor(y_data))
-#    
-#    # get vector sequences for video frames
-#    # sequences in hist_diffs_train 
-#    
-#    # Prepare the RNN 
-##    # Instantiate RNN model
-#    rnn = RNN(num_classes, input_size, hidden_size, num_layers)
-#    print(rnn)
-#    
-#    # Set loss and optimizer function
-#    # CrossEntropyLoss = LogSoftmax + NLLLoss
-#    criterion = torch.nn.CrossEntropyLoss()
-#    optimizer = torch.optim.Adam(rnn.parameters(), lr=0.1)
-#    
-#    # Train the model
-#    for epoch in range(10):
-#        outputs = rnn(inputs)
-#        optimizer.zero_grad()
-#        loss = criterion(outputs, labels)
-#        loss.backward()
-#        optimizer.step()
-#        _, idx = outputs.max(1)
-#        idx = idx.data.numpy()
-#        #result_str = [idx2char[c] for c in idx.squeeze()]
-#        
-#        print("epoch: %d, loss: %1.3f" % (epoch + 1, loss.data[0]))
-#        #print("Predicted string: ", ''.join(idx))
-#    
-#    print("Learning finished!")    
+
+
 
 #    for idx, sample in enumerate(hist_diffs_train):
 #        #sample = add_feature(sample, cuts_lst[idx])     # add feature #Frames since last CUT
